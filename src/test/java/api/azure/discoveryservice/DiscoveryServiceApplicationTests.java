963abe4fda74d22e9ac91245e04d108c5c43e686
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-08-14 21:42:39 -0500

    first commit

---
+++ src/test/java/api/azure/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
